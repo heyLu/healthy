7cf1d2920cde513facd2ac22d51859bd3e283967
Luna Stadler <lu@papill0n.org>
2021-03-24 19:19:46 +0100

    Initial commit: It works (sometimes!)

---
+++ README.md
@@ -0,0 +1,11 @@
+# healthy - a tiny Linux process monitor
+
+Inspired by [Vitals](https://hmarr.com/blog/vitals/), I present to you
+`healthy`, which attempts to do the same, only for Linux.
+
+![screenshot of healthy running](./screenshot.png)
+
+## License
+
+`healthy` is licensed under GPLv3, see [`LICENSE`](./LICENSE) for
+details.
---
+++ healthy.py
@@ -0,0 +1,164 @@
+import os
+import random
+import time
+
+import gi
+gi.require_version("GLib", "2.0")
+gi.require_version("Gdk", "3.0")
+gi.require_version("Gtk", "3.0")
+from gi.repository import GLib
+from gi.repository import Gdk
+from gi.repository import Gtk
+
+class CPUGraph(Gtk.Box):
+    def __init__(self, num_samples, name, cpu_usage):
+        Gtk.Box.__init__(self)
+
+        self.num_samples = num_samples
+        self.name = name
+        self.cpu_usage = cpu_usage
+
+        self.label = Gtk.Label()
+        self.label.set_max_width_chars(20)
+
+        self.drawing_area = Gtk.DrawingArea()
+        self.drawing_area.set_size_request(width=300, height=30)
+        self.drawing_area.connect("draw", self.on_draw)
+
+        self.pack_start(self.label, True, True, 5)
+        self.pack_end(self.drawing_area, False, True, 0)
+
+    def on_draw(self, widget, cairo_context):
+        self.label.set_text(self.name)
+
+        width, height = self.drawing_area.get_size_request()
+
+        # white background
+        cairo_context.set_source_rgb(1, 1, 1)
+        cairo_context.rectangle(0, 0, width, height)
+        cairo_context.fill()
+
+        # squiggly lines!
+        cairo_context.set_source_rgb(0.3, 0.3, 0.7)
+        for idx, cpu in enumerate(self.cpu_usage):
+            cairo_context.line_to(idx*(width/self.num_samples), height - cpu*(height/100))
+        cairo_context.stroke()
+
+class CPUGraphCollection(Gtk.Box):
+    def __init__(self, sample_seconds):
+        Gtk.Box.__init__(self, orientation="vertical")
+
+        self.sample_seconds = sample_seconds
+        self.num_samples = int(60 / self.sample_seconds)
+
+        self.cpu_graphs = []
+        for _ in range(20):
+            cpu_graph = CPUGraph(self.num_samples, "", [])
+            self.pack_start(cpu_graph, True, True, 5)
+            self.cpu_graphs.append(cpu_graph)
+
+        self.cpu = {}
+        self.update()
+
+    def update(self):
+        global_cpu = read_global_stat()
+        top_20_pids = cpu_stats(self.sample_seconds)
+        cpu_usages = []
+        for pid in top_20_pids:
+            if pid not in self.cpu:
+                self.cpu[pid] = [0]*self.num_samples
+
+            self.cpu[pid].append(pid.cpu_usage)
+            if len(self.cpu[pid]) > self.num_samples:
+                self.cpu[pid].pop(0)
+
+        for pid in self.cpu:
+            if pid not in top_20_pids:
+                self.cpu[pid].append(0)
+                if len(self.cpu[pid]) > self.num_samples:
+                    self.cpu[pid].pop(0)
+
+            cpu_usages.append((pid, self.cpu[pid]))
+
+        cpu_usages = list(self.cpu.items())
+        cpu_usages.sort(key=lambda u: sum(u[1]), reverse=True)
+        cpu_usages = cpu_usages[:20]
+        for i, cpu_usage in enumerate(cpu_usages):
+            self.cpu_graphs[i].name = cpu_usage[0].tcomm
+            self.cpu_graphs[i].cpu_usage = cpu_usage[1]
+            self.cpu_graphs[i].queue_draw()
+
+        GLib.idle_add(self.update)
+
+class PIDStat():
+    def __init__(self, stat_line):
+        name_start, name_end = stat_line.index('('), stat_line.index(')')
+        name = stat_line[name_start+1:name_end]
+        stat_line = stat_line[:name_start] + stat_line[name_end+2:]
+        self.fields = stat_line.split(" ")
+        self.fields.insert(1, name)
+
+        self.pid = int(self.fields[0])
+        self.tcomm = self.fields[1]
+        self.utime = int(self.fields[13])
+        self.stime = int(self.fields[14])
+
+        self.cpu_usage = 0.0
+
+    def __getitem__(self, idx):
+        return self.fields[idx]
+
+    def __repr__(self):
+        return f'PIDStat({self.pid}, "{self.tcomm}")'
+
+    def __hash__(self):
+        return hash((self.pid, self.tcomm))
+
+    def __eq__(self, other):
+        return (self.pid, self.tcomm) == (other.pid, other.tcomm)
+# https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/Documentation/filesystems/proc.rst
+def read_stat(pid):
+    with open("/proc/"+pid+"/stat") as f:
+        stat_line = f.readline().strip()
+        return PIDStat(stat_line)
+
+def read_global_stat():
+    # user + nice + system + idle + iowait + irq + softirq + steal
+    with open("/proc/stat") as f:
+        global_stat = [int(x) for x in f.readline().strip()[len("cpu  "):].split(" ")]
+        return sum(global_stat[:8])
+
+# inspired by https://github.com/scaidermern/top-processes/blob/master/top_proc.c
+def cpu_stats(n=20, sample_seconds=1.0):
+    global_cpu = read_global_stat()
+    pid_stats_before = dict([(pid, read_stat(pid)) for pid in os.listdir("/proc") if pid.isnumeric()])
+    time.sleep(sample_seconds)
+    pid_stats_after = dict([(pid, read_stat(pid)) for pid in os.listdir("/proc") if pid.isnumeric()])
+    global_cpu = read_global_stat() - global_cpu
+
+    pid_stats = []
+    for pid in pid_stats_after:
+        if pid in pid_stats_before:
+            pid_before = pid_stats_before[pid]
+            pid_after = pid_stats_after[pid]
+            cpu_time = (pid_after.utime + pid_after.stime) - (pid_before.utime + pid_before.stime)
+            pid_after.cpu_usage = (cpu_time / global_cpu) * 100.0 * os.cpu_count()
+
+            pid_stats.append(pid_after)
+
+    pid_stats.sort(key=lambda stat: stat.cpu_usage, reverse=True)
+    return pid_stats[:20]
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    win.set_keep_above(True)
+
+    cpu_graphs = CPUGraphCollection(0.5)
+
+    win.add(cpu_graphs)
+    win.show_all()
+
+if __name__ == '__main__':
+    app = Gtk.Application(application_id='org.papill0n.Healthy')
+    app.connect('activate', on_activate)
+    app.run(None)
